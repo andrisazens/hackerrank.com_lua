2a3b57994412aec1a40cba9b09b7622a787a44be
Andris Azens <andris.azens@inbox.lv>
2016-12-09 21:06:05 +0200

    Finished day 3

--- 30_days_of_code/03_cond.lua
+++ 30_days_of_code/03_cond.lua
@@ -1,10 +1,18 @@
 --[[Given an integer, , perform the following conditional actions:
 If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird]]--
+If  is even and in the inclusive range of 2 to 5, print Not Weird
+If  is even and in the inclusive range of 6 to 20, print Weird
+If  is even and greater than 20, print Not Weird]]--
 
 N = io.read("*number", "*l")
 
-if 
+if (N % 2 ~= 0) then
+    print("Weird")
+elseif (N >= 2 and N <= 5) then
+    print("Not Weird")
+elseif (N >= 6 and N <= 20) then
+    print("Weird")
+else
+    print("Not Weird")
+end
 
