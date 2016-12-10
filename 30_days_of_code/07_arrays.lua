05b9927fc5e7c1f491e684540b998e623cfb4813
Andris Azens <andris.azens@inbox.lv>
2016-12-10 10:23:27 +0200

    Day 7 solved

--- 30_days_of_code/07_arrays.lua
+++ 30_days_of_code/07_arrays.lua
@@ -8,3 +8,13 @@ for token in string.gmatch(arr_temp, "[^%s]+") do
    arr[arr_i] = token
    arr_i = arr_i + 1
 end
+
+local result = ""
+for i = #arr, 1, -1 do    
+    result = result .. arr[i]
+    if (i > 1) then
+       result = result .. " "    
+    end
+end
+
+print(result)
